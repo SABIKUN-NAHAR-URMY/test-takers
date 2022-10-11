c7fd8835e330ba4c5e56c31b8f4ee911222be06f
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-10-11 19:56:39 +0600

    blog section update

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -8,13 +8,13 @@ const Blog = () => {
                 <p className='text-xl'><strong>Answer: </strong> At its heart, React Router is a state container for the current location , or URL. It keeps track of the location and renders different Route's as it changes, and it also gives you tools to update the location using Link's and the history API.</p>
             </div>
             <div className='border-2 border-slate-400 rounded-lg m-4 p-5'>
-                <h1 className='text-3xl font-semibold'><strong>Question-1:</strong> How does context api works?</h1>
-                <p>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
+                <h1 className='text-3xl font-semibold'><strong>Question-2:</strong> How does context api works?</h1>
+                <p className='text-xl'><strong>Answer: </strong>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
             </div>
 
             <div className='border-2 border-slate-400 rounded-lg m-4 p-5'>
-                <h1 className='text-3xl font-semibold'><strong>Question-1:</strong> What is useRef hook in React??</h1>
-                <p>The useRef Hook allows you to persist values between renders. It can be used to store a mutable value that does not cause a re-render when updated. It can be used to access a DOM element directly.useRef returns a mutable ref object whose .current property is initialized to the passed argument ( initialValue ). The returned object will persist for the full lifetime of the component. Essentially, useRef is like a “box” that can hold a mutable value in its .current property.</p>
+                <h1 className='text-3xl font-semibold'><strong>Question-3:</strong> What is useRef hook in React??</h1>
+                <p className='text-xl'><strong>Answer: </strong>The useRef Hook allows you to persist values between renders. It can be used to store a mutable value that does not cause a re-render when updated. It can be used to access a DOM element directly.useRef returns a mutable ref object whose .current property is initialized to the passed argument ( initialValue ). The returned object will persist for the full lifetime of the component. Essentially, useRef is like a “box” that can hold a mutable value in its .current property.</p>
             </div>
 
         </div>
