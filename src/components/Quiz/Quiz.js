3f9b0e1c75b4176bbdf74e8a4babec36e74d8f11
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-10-11 22:29:11 +0600

    quiz name uploaded

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -3,16 +3,18 @@ import QuizDetails from '../QuizDetails/QuizDetails';
 
 const Quiz = () => {
     const quiz = useLoaderData();
-    const {name} = quiz.data;
-    console.log(name);
     return (
-            <div>
+        <div>
+            <div className='text-2xl font-semibold mt-12'>
             {
-                quiz.data.questions.map((quizData,idx,name) => <QuizDetails 
-                key={quizData.id}
-                quizData={quizData}
-                name={name}
-                id={idx}></QuizDetails>)
+                <h1>Quiz of {quiz.data.name}</h1>
+            }
+            </div>
+            {
+                quiz.data.questions.map((quizData, idx) => <QuizDetails
+                    key={quizData.id}
+                    quizData={quizData}
+                    id={idx}></QuizDetails>)
             }
         </div>
     );
--- src/components/QuizDetails/QuizDetails.js
+++ src/components/QuizDetails/QuizDetails.js
@@ -1,12 +1,10 @@
 import Option from '../Option/Option';
 
-const QuizDetails = ({ quizData,name,id }) => {
+const QuizDetails = ({ quizData,id }) => {
 
     const {question, correctAnswer, options } = quizData;
-    // const{name} = name;
     return (
         <div className='border-2 border-indigo-600 m-5'>
-            {name}
             <h2 className='text-xl font-bold'>Quiz-{id}:{question}</h2>
             <div className='grid grid-cols-2'>
             {
