37476f710bc3a87c2d9915a56ac31f38f9a5a055
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-10-12 00:16:53 +0600

    toast add done for eye button

--- src/components/QuizDetails/QuizDetails.js
+++ src/components/QuizDetails/QuizDetails.js
@@ -1,14 +1,22 @@
 import Option from '../Option/Option';
 import { EyeIcon } from '@heroicons/react/24/solid';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const QuizDetails = ({ quizData, id }) => {
-
     const { question, correctAnswer, options } = quizData;
+    const showToastMessage = () => {
+        toast.success(`Answer: ${correctAnswer}`, {
+            position: toast.POSITION.TOP_RIGHT
+        });
+    };
     return (
         <div className='border-2 border-slate-400 rounded-lg p-10 m-10 mx-auto w-[50%]'>
             <div className='flex'>
                 <h2 className='text-xl font-bold'>Quiz-{id}:{question}</h2>
-                <span className='cursor-pointer'><EyeIcon className='w-6 h-6'></EyeIcon></span>
+                <button><EyeIcon onClick={showToastMessage} className='w-6 h-6'></EyeIcon></button>
+                <ToastContainer />
             </div>
 
             <div className='grid md:grid-cols-2 text-start p-4 ml-11'>
