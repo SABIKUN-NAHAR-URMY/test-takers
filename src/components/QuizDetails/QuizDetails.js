ef8e29cd18d51327cab4d21f9172089af9afac53
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-10-11 23:32:33 +0600

    eye icon set up

--- src/components/QuizDetails/QuizDetails.js
+++ src/components/QuizDetails/QuizDetails.js
@@ -1,19 +1,24 @@
 import Option from '../Option/Option';
+import { EyeIcon } from '@heroicons/react/24/solid';
 
-const QuizDetails = ({ quizData,id }) => {
+const QuizDetails = ({ quizData, id }) => {
 
-    const {question, correctAnswer, options } = quizData;
+    const { question, correctAnswer, options } = quizData;
     return (
         <div className='border-2 border-slate-400 rounded-lg p-10 m-10 mx-auto w-[50%]'>
-            <h2 className='text-xl font-bold'>Quiz-{id}:{question}</h2>
+            <div className='flex'>
+                <h2 className='text-xl font-bold'>Quiz-{id}:{question}</h2>
+                <span className='cursor-pointer'><EyeIcon className='w-6 h-6'></EyeIcon></span>
+            </div>
+
             <div className='grid md:grid-cols-2 text-start p-4 ml-11'>
-            {
-                 options.map((option,idx) => <Option 
-                key={idx}
-                option={option}
-                ></Option>)
-                
-            }
+                {
+                    options.map((option, idx) => <Option
+                        key={idx}
+                        option={option}
+                    ></Option>)
+
+                }
             </div>
         </div>
     );
--- src/components/QuizTopic/QuizTopic.js
+++ src/components/QuizTopic/QuizTopic.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ArrowRightCircleIcon } from '@heroicons/react/24/solid'
+import { ArrowRightCircleIcon } from '@heroicons/react/24/solid';
 import { Link } from 'react-router-dom';
 
 const QuizTopic = ({quizTopic}) => {
