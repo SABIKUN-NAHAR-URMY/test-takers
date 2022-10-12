c8987fdfaad81a6525daa3d9d01b550f7aa79989
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-10-12 13:01:42 +0600

    statistics update

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -6,7 +6,7 @@ const Statistics = () => {
     const data = useLoaderData();
     return (
         <div className='mt-[100px] md:mt-0'>
-            <LineChart className='w-[100%] md:mx-auto md:pt-28' width={500} height={300} data={data.data}>
+            <LineChart className='md:mx-auto md:pt-28' width={500} height={300} data={data.data}>
                 <Line type="monotone" dataKey="total" stroke="#82ca9d" />
                 <XAxis dataKey="name" />
                 <YAxis />
