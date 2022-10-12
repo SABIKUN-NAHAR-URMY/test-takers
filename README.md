c8123937be9c9036148fbf578dcd685241cfec82
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-10-12 16:49:25 +0600

    upadate all again

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 * Loader for api data load
 * Uses Route system
 * HeroIcons uses 
-# In this site ready for online base quiz system.
+# This site ready for online base quiz system.
 # Here uses : 
 * Navbar
 * Home page
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -5,7 +5,7 @@ import { LineChart, Line, XAxis, YAxis, Tooltip} from 'recharts';
 const Statistics = () => {
     const data = useLoaderData();
     return (
-        <div className='mt-[100px] md:mt-0'>
+            <div className='mt-[100px] md:mt-0'>
             <LineChart className='md:mx-auto md:pt-28' width={500} height={300} data={data.data}>
                 <Line type="monotone" dataKey="total" stroke="#82ca9d" />
                 <XAxis dataKey="name" />
